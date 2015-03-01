5d19e23e79ad314eb215e9cf39ee6b30c5e69e69
O01eg <o01eg@yandex.ru>
2015-03-01 19:28:40 +0300

    Tutorial 03. Matrices.

--- src/libtutcommon/lib.rs
+++ src/libtutcommon/lib.rs
@@ -1,11 +1,18 @@
 #![deny(deprecated)]
 #![deny(missing_docs)]
+#![deny(non_snake_case)]
+#![deny(non_upper_case_globals)]
+
+#![feature(fs, io, path, std_misc)]
 
 #![doc = "Common stuff for tutorials."]
 #![crate_name = "tutcommon"]
 
+use std::default::Default;
 use std::fs::File;
 use std::io::Read;
+use std::num::Float;
+use std::ops::Mul;
 use std::path::AsPath;
 
 #[doc = "Read content of file into string."]
@@ -13,5 +20,123 @@ pub fn read_source_from_file<P: AsPath + ?Sized>(path : &P) -> String {
     let mut res = String::new();
     File::open(path).unwrap().read_to_string(&mut res).unwrap();
     return res;
+}
+
+#[doc = "Vector with 3 components (x,y,z)"]
+pub struct Vector3f (pub f32, pub f32, pub f32);
+
+impl Vector3f {
+    #[doc = "Normalize vector to length 1."]
+    pub fn normalize(&self) -> Vector3f {
+        let l = (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt();
+
+        Vector3f(self.0 / l, self.1 / l, self.2 / l)
+    }
+
+    #[doc = "Cross product."]
+    pub fn cross(&self, _rhs:&Vector3f) -> Vector3f {
+        Vector3f (
+            self.1 * _rhs.2 - self.2 * _rhs.1,
+            self.2 * _rhs.0 - self.0 * _rhs.2,
+            self.0 * _rhs.1 - self.1 * _rhs.0,
+        )
+    }
+
+    fn sub(&self, _rhs:&Vector3f) -> Vector3f {
+        Vector3f (
+            self.0 - _rhs.0,
+            self.1 - _rhs.1,
+            self.2 - _rhs.2,
+        )
+    }
+}
+
+#[doc = "Matrix 4x4 to send into OpenGL."]
+#[derive(Debug)]
+pub struct Matrix4f {
+    data : [[f32; 4]; 4], //column major order data[j][i] points to j-th column i-th row.
+}
+
+impl Matrix4f {
+    #[doc = "Get raw data for OpenGL."]
+    pub fn as_ptr(&self) -> * const f32 {
+        self.data[0].as_ptr()
+    }
+
+    #[doc = "Get perspective projection matrix."]
+    pub fn perspective(fov: f32, aspect: f32, znear: f32, zfar: f32) -> Matrix4f {
+        let mut res : Matrix4f = Default::default();
+
+        let f = 1.0 / (fov / 2.0 ).to_radians().tan();
+
+        res.data[0][0] = f / aspect;
+        res.data[1][1] = f;
+        res.data[2][2] = (zfar + znear) / (znear - zfar);
+        res.data[2][3] = - 1.0;
+        res.data[3][2] = 2.0 * zfar * znear / (znear - zfar);
+        res.data[3][3] = 0.0;
+
+        res
+    }
+
+    #[doc = "Generate matrix for camera."]
+    pub fn look_at(eye:Vector3f, center:Vector3f, up:Vector3f) -> Matrix4f {
+        let f = center.sub(&eye).normalize();
+        let up1 = up.normalize();
+        let s = f.cross(&up1).normalize();
+        let u = s.cross(&f);
+
+        let mut m : Matrix4f = Default::default();
+        m.data[0][0] = s.0;
+        m.data[1][0] = s.1;
+        m.data[2][0] = s.2;
+
+        m.data[0][1] = u.0;
+        m.data[1][1] = u.1;
+        m.data[2][1] = u.2;
+
+        m.data[0][2] = -f.0;
+        m.data[1][2] = -f.1;
+        m.data[2][2] = -f.2;
+
+        let mut t : Matrix4f = Default::default();
+
+        t.data[3][0] = -eye.0;
+        t.data[3][1] = -eye.1;
+        t.data[3][2] = -eye.2;
+
+        m * t
+    }
+}
+
+impl Default for Matrix4f {
+    fn default() -> Matrix4f {
+        Matrix4f { data : [
+            [1.0, 0.0, 0.0, 0.0,],
+            [0.0, 1.0, 0.0, 0.0,],
+            [0.0, 0.0, 1.0, 0.0,],
+            [0.0, 0.0, 0.0, 1.0,],
+        ] }
+    }
+}
+
+impl Mul for Matrix4f {
+    type Output = Matrix4f;
+
+    fn mul(self, _rhs: Matrix4f) -> Matrix4f {
+        let mut res : Matrix4f = Default::default();
+
+        for i in 0..4 { // row
+            for j in 0..4 { // column
+                let mut sum = 0.0;
+                for k in 0..4 {
+                    sum += self.data[k][i] * _rhs.data[j][k];
+                }
+                res.data[j][i] = sum;
+            }
+        }
+
+        res
+    }
 }
 
---
+++ src/tut03/glscene.rs
@@ -0,0 +1,201 @@
+use std;
+use std::path::AsPath;
+
+use gl;
+use gl::types::{GLchar, GLfloat, GLint, GLuint};
+
+use tutcommon;
+
+// An array of 3 vectors which represents 3 vertices.
+static G_VERTEX_BUFFER_DATA : [GLfloat; 9] = [
+    -1.0, -1.0, 0.0,
+    1.0, -1.0, 0.0,
+    0.0, 1.0, 0.0
+];
+
+#[doc = "Moved out drawing GL stuff to avoid mess with the other code."]
+pub struct GLScene {
+    vertex_array_id : GLuint, //VAO id.
+    vertex_buffer_id : GLuint, //VBO id.
+    program_id : GLuint, //Shader program id.
+    matrix_id : GLint, // MVP uniform locaion.
+    mvp : tutcommon::Matrix4f, // Matrix 
+}
+
+impl GLScene {
+
+    #[doc = "Create scene and init it."]
+    pub fn new() -> GLScene {
+
+        let mut vertex_array_id = 0;
+        
+        unsafe {
+            // create Vertex Array Object and set it as the current one:
+            gl::GenVertexArrays(1, &mut vertex_array_id);
+            gl::BindVertexArray(vertex_array_id);
+        }
+
+        let mut vertex_buffer_id = 0;
+
+        // Create and compile our GLSL program from the shaders
+        let program_id = GLScene::load_program("data/tut03/SimpleTransform.vertexshader"
+            , "data/tut03/SingleColor.fragmentshader");
+
+        let mut matrix_id = 0;
+
+        unsafe {
+            // Get a handle for our "MVP" uniform 
+            matrix_id = gl::GetUniformLocation(program_id, "MVP".as_ptr() as * const i8);
+        }
+
+        // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+        let projection : tutcommon::Matrix4f = tutcommon::Matrix4f::perspective(45.0, 4.0 / 3.0, 0.1, 100.0);
+
+        println!("Projection matrix: {:?}", projection);
+        
+        // Camera matrix        
+        let view = tutcommon::Matrix4f::look_at(
+            tutcommon::Vector3f(4.0, 3.0, 3.0), // Camera is at (4,3,3), in World Space
+            tutcommon::Vector3f(0.0, 0.0, 0.0), // and looks at the origin
+            tutcommon::Vector3f(0.0, 1.0, 0.0) // Head is up (set to 0,-1,0 to look upside-down)
+        );
+
+        println!("View matrix: {:?}", view);
+
+        // Model matrix : an identity matrix (model will be at the origin)
+        let model = std::default::Default::default();
+
+        // Our ModelViewProjection : multiplication of our 3 matrices        
+        let mvp = projection * view * model; // Remember, matrix multiplication is the other way around
+
+        println!("MVP matrix: {:?}", mvp);
+        
+        unsafe {
+            // Generate 1 buffer.
+            gl::GenBuffers(1, &mut vertex_buffer_id);
+
+            // Set it current.
+            gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer_id);
+
+            // Send vertices to buffer.
+            gl::BufferData(gl::ARRAY_BUFFER
+                , std::mem::size_of_val(&G_VERTEX_BUFFER_DATA) as i64
+                , std::mem::transmute(&G_VERTEX_BUFFER_DATA)
+                , gl::STATIC_DRAW);
+        }
+       
+        GLScene { vertex_array_id : vertex_array_id
+            , vertex_buffer_id : vertex_buffer_id
+            , program_id : program_id
+            , matrix_id : matrix_id
+            , mvp : mvp }
+    }
+
+    #[doc = "Render scene each frame."]
+    pub fn draw(&self) {
+
+        unsafe {
+            //1st attribute buffer : vertices
+            gl::EnableVertexAttribArray(0);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.vertex_buffer_id);
+            gl::VertexAttribPointer(
+                0, // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                3, // size
+                gl::FLOAT, // type
+                gl::FALSE, // normalized?
+                0, // stride
+                std::ptr::null() // array buffer offset
+            );
+
+            // Use our shader
+            gl::UseProgram(self.program_id);
+
+            // Send our transformation to the currently bound shader,
+            // in the "MVP" uniform.
+            gl::UniformMatrix4fv(self.matrix_id, 1, gl::FALSE, self.mvp.as_ptr());
+
+            // Draw the triangle!
+            // Starting from vertex 0; 3 vertices total -> 1 triangle.
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+
+            gl::DisableVertexAttribArray(0);
+        }
+    }
+
+    fn load_program<VP: AsPath + ?Sized, FP: AsPath + ?Sized>(vertex_file_path : &VP, fragment_file_path : &FP) -> GLuint {
+        unsafe {
+            // Create the shaders.
+            let vertex_shader_id = gl::CreateShader(gl::VERTEX_SHADER);
+            let fragment_shader_id = gl::CreateShader(gl::FRAGMENT_SHADER);
+        
+            // Read the Vertex Shader code from the file.
+            let vertex_shader_code = tutcommon::read_source_from_file(vertex_file_path);
+        
+            // Read the Fragment Shader code from the file
+            let fragment_shader_code = tutcommon::read_source_from_file(fragment_file_path);
+
+            // Compile Vertex Shader
+            println!("Compiling shader: {}", vertex_file_path.as_path().to_string_lossy());
+            GLScene::compile_and_check_shader(vertex_shader_id, &vertex_shader_code[..]);
+
+            // Compile Fragment Shader
+            println!("Compiling shader: {}", fragment_file_path.as_path().to_string_lossy());
+            GLScene::compile_and_check_shader(fragment_shader_id, &fragment_shader_code[..]);
+
+            // Link the program
+            let program_id = gl::CreateProgram();
+            gl::AttachShader(program_id, vertex_shader_id);
+            gl::AttachShader(program_id, fragment_shader_id);
+            gl::LinkProgram(program_id);
+
+            let mut result = 0;
+            let mut info_log_length = 0;
+
+            // Check the program
+            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut result);
+            gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut info_log_length);
+            let mut buf = Vec::<GLchar>::with_capacity((info_log_length + 1) as usize);
+            buf.set_len((info_log_length + 1) as usize);
+            gl::GetProgramInfoLog(program_id, info_log_length + 1, std::ptr::null_mut(), buf[..].as_mut_ptr());
+            println!("Program link log: {}", String::from_utf8_lossy(std::mem::transmute::<&[i8],&[u8]>(&buf[..])));
+
+            gl::DeleteShader(vertex_shader_id);
+            gl::DeleteShader(fragment_shader_id);
+
+            program_id
+        }
+    }
+
+    fn compile_and_check_shader(shader_id : GLuint, shader_source : &str) {
+        let mut result = 0;
+        let mut info_log_length = 0;
+        unsafe {
+            let source : &[i8] = std::mem::transmute(shader_source);
+            // Compile Shader
+            gl::ShaderSource(shader_id, 1, std::mem::transmute(&source), std::ptr::null());
+            gl::CompileShader(shader_id);
+
+            // Check Shader
+            gl::GetShaderiv(shader_id, gl::COMPILE_STATUS, &mut result);
+            gl::GetShaderiv(shader_id, gl::INFO_LOG_LENGTH, &mut info_log_length);
+            let mut buf = Vec::<GLchar>::with_capacity((info_log_length + 1) as usize);
+            buf.set_len((info_log_length + 1) as usize);
+            gl::GetShaderInfoLog(shader_id, info_log_length + 1, std::ptr::null_mut(), buf[..].as_mut_ptr());
+            println!("Shader compile log: {}", String::from_utf8_lossy(std::mem::transmute::<&[i8],&[u8]>(&buf[..])));
+        }
+    }
+}
+
+#[doc = "Always clean up after yourself."]
+impl Drop for GLScene {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteProgram(self.program_id);
+
+            gl::DeleteBuffers(1, &self.vertex_buffer_id);
+
+            gl::DeleteVertexArrays(1, &self.vertex_array_id);
+        }
+    }
+}
+
